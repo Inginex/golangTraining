685f1b1206484a76fa09ec70cd2e499e53bd5bae
Inginex <samuelpalmeira@outlook.com>
2017-11-26 14:46:00 -0200

    Incrementor challenge solved

--- 23_go-routines/14_incrementor-challenge/01_description/main.go
+++ 23_go-routines/14_incrementor-challenge/01_description/main.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"sync/atomic"
 )
 
-var count int64
-var wg sync.WaitGroup
-
 func main() {
-	wg.Add(2)
-	go incrementor("1")
-	go incrementor("2")
-	wg.Wait()
-	fmt.Println("Final Counter:", count)
+	var count int
+	c := incrementor(2)
+
+	for n := range c {
+		count++
+		fmt.Println(n)
+	}
+
+	fmt.Println("Final count: ", count)
 }
 
-func incrementor(s string) {
-	for i := 0; i < 20; i++ {
-		atomic.AddInt64(&count, 1)
-		fmt.Println("Process: "+s+" printing:", i)
+func incrementor(n int) chan string {
+	out := make(chan string)
+	done := make(chan bool)
+
+	// PROCESS
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			// COUNTING
+			for j := 0; j < 20; j++ {
+				out <- fmt.Sprint("Process: ", i, " printing: ", j)
+			}
+			done <- true
+		}(i)
 	}
-	wg.Done()
+
+	go func() {
+		for i := 0; i < n; i++ {
+			<-done
+		}
+		close(out)
+	}()
+
+	return out
 }
 
 /*
