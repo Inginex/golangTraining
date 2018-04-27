0502d571249d6159234c26eb63d358d44e21ea13
Samuel Palmeira <samuelpalmeira@outlook.com>
2018-04-27 13:53:16 -0300

    Adds assertion type example

--- 00_Hands-on-exercises/#11_Level11/03/main.go
+++ 00_Hands-on-exercises/#11_Level11/03/main.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 type customErr struct {
-	error
+	Info string
+	cerr error
+}
+
+func (err customErr) Error() string {
+	return fmt.Sprintf("Info error: %s", err.Info)
 }
 
 func main() {
-	cErr := customErr{fmt.Errorf("This is a custom error")}
+	cErr := customErr{"info string", fmt.Errorf("This is a custom error")}
 	foo(cErr)
 }
 
 func foo(err error) {
+	fmt.Println(err.(customErr).cerr)
 	fmt.Println(err)
 }
